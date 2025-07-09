e44d8c7d79853dc19e9c61f35e755d1b8b0bf3c7
Barcha22 <bmuneeb2050@gmail.com>
2025-07-09 23:01:39 +0500

    initial commit

---
+++ String_Interpolations.dart
@@ -0,0 +1,15 @@
+void main() {
+  var name = "Muneeb";
+  var msg = "Hi, my name is " + name;
+  var msg2 = "Hi, my name is $name";
+  print(msg);
+  print(msg2);
+
+  print("The length of the message is : ${msg.length}");
+  print("The length of the name is : ${name.length}");
+
+  var a = 5;
+  var b = 2;
+  print("The sum of a and b is : ${a + b}");
+  print("${a} times ${b} is : ${a * b}");
+}
---
+++ basics.dart
@@ -0,0 +1,38 @@
+void main() {
+  // print("Hello World.");
+
+  //Comments
+  // Single line comments
+  /*Multi
+  line
+   Comments
+   */
+  /// document comments
+
+  // // Data Types
+  // var name = "Muneeb";
+  // print(name);
+
+  // String name1 = "Kalam";
+  // print(name1);
+
+  // int n = 5;
+  // print(n);
+
+  // bool value = true;
+  // print(value);
+
+  // var a = 6;
+  // print(a);
+
+  // var b = 5.55555;
+  // print(b);
+
+  // dynamic pi = 3.14;
+  // dynamic percentage = 90.6;
+  // print(pi);
+  // print(percentage);
+
+  // double num = 5.45
+  //print(num);
+}
